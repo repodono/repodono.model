ac43573a5f93502e10e6adf18c415e8532693e3c
Tommy Yu <tommy.yu@auckland.ac.nz>
2019-02-18 14:14:36 +1300

    License should be GPLv3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version = '0.0'
 
 classifiers = """
 Development Status :: 3 - Alpha
-License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
 Operating System :: OS Independent
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7

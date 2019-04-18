9d187e759019ffec96676b9caf7ac800d59f12b1
Tommy Yu <tommy.yu@auckland.ac.nz>
2019-04-18 18:31:02 +1200

    Implement a naive router
    - Just putting this in the urimatch module for the mean time.

--- src/repodono/model/tests/test_urimatch.py
+++ src/repodono/model/tests/test_urimatch.py
@@ -7,6 +7,7 @@ from repodono.model.urimatch import template_to_regex_patternstr
 from repodono.model.urimatch import check_variable
 from repodono.model.urimatch import match
 from repodono.model.urimatch import URITemplateMatcher
+from repodono.model.urimatch import URITemplateRouter
 
 
 class BaseTestCase(unittest.TestCase):
@@ -140,3 +141,64 @@ class UriMatchTestCase(unittest.TestCase):
         self.assertEqual({
             'root': 'value',
         }, result)
+
+
+class URITemplateRouterTestCase(unittest.TestCase):
+
+    def test_standard_construction(self):
+        router = URITemplateRouter([
+            URITemplate('/e/{target}'),
+            URITemplate('/e/{target}{/path*}'),
+            URITemplate('/e/{target}{/path*}/alternate/{view}'),
+            URITemplate('/w/{target}{/path*}'),
+            URITemplate('/w/{target}{/path*}/view'),
+            URITemplate('/w/{target}{/path*}/index'),
+        ])
+
+        self.assertEqual(('/e/{target}', {
+            'target': 'hello',
+        }), router('/e/hello'))
+
+        self.assertEqual(('/e/{target}{/path*}', {
+            'target': 'a_target',
+            'path': ['some', 'path', 'down', 'the', 'line'],
+        }), router('/e/a_target/some/path/down/the/line'))
+
+        self.assertEqual(('/e/{target}{/path*}/alternate/{view}', {
+            'view': 'some_view',
+            'target': 'a_target',
+            'path': ['short', 'path'],
+        }), router('/e/a_target/short/path/alternate/some_view'))
+
+        self.assertEqual(('/w/{target}{/path*}', {
+            'target': 'other_target',
+            'path': ['some', 'other', 'path'],
+        }), router('/w/other_target/some/other/path'))
+
+        self.assertEqual(('/w/{target}{/path*}/view', {
+            'target': 'other_target',
+            'path': ['some', 'other', 'path'],
+        }), router('/w/other_target/some/other/path/view'))
+
+        self.assertIsNone(router('/e/+wat'))
+        self.assertIsNone(router('/z/nowhere/short/path/alternate/some_view'))
+        self.assertIsNone(router('/w/not_enough'))
+
+    def test_from_list_of_strings(self):
+        router = URITemplateRouter.from_strings([
+            '/e/{target}',
+            '/e/{target}{/path*}',
+            '/e/{target}{/path*}/alternate/{view}',
+            '/w/{target}{/path*}',
+            '/w/{target}{/path*}/view',
+            '/w/{target}{/path*}/index',
+        ])
+
+        self.assertEqual(('/e/{target}', {
+            'target': 'goodbye',
+        }), router('/e/goodbye'))
+
+        self.assertEqual(('/w/{target}{/path*}/index', {
+            'target': 'primary',
+            'path': ['path', 'to', 'the'],
+        }), router('/w/primary/path/to/the/index'))
--- src/repodono/model/urimatch.py
+++ src/repodono/model/urimatch.py
@@ -1,5 +1,7 @@
 import regex
+from operator import attrgetter
 from functools import partial
+from uritemplate import URITemplate
 from uritemplate.variable import URIVariable
 
 
@@ -237,3 +239,36 @@ def match(template, uri, __cache={}):
     if template.uri not in __cache:
         __cache[template.uri] = URITemplateMatcher(template)
     return __cache[template.uri](uri)
+
+
+class URITemplateRouter(object):
+    """
+    A naive implementation of a router that would route based on a set
+    of provided URITemplates through the URITemplateMatcher class above.
+    """
+
+    def __init__(self, uritemplates):
+        """
+        Arguments
+
+        uritemplates
+            A list of URITemplate objects.
+        """
+
+        self.matchers = [URITemplateMatcher(template) for template in sorted(
+            uritemplates, key=attrgetter('uri'), reverse=True)]
+
+    @classmethod
+    def from_strings(cls, uritemplate_strs):
+        """
+        Construct a router from a list of strings that can be formed
+        into a valid URITemplate.
+        """
+
+        return cls(URITemplate(s) for s in uritemplate_strs)
+
+    def __call__(self, uri):
+        for matcher in self.matchers:
+            result = matcher(uri)
+            if result is not None:
+                return matcher.template.uri, result

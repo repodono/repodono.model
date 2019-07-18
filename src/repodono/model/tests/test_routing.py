0236205e8600d008bea102921a4352f14da2cdb2
Tommy Yu <tommy.yu@auckland.ac.nz>
2019-07-18 20:57:08 +1200

    Reworked the routing test case
    - Provide a contained test method to test for equality.

--- src/repodono/model/tests/test_routing.py
+++ src/repodono/model/tests/test_routing.py
@@ -6,8 +6,11 @@ from repodono.model.routing import URITemplateRouter
 
 class URITemplateRouterTestCase(unittest.TestCase):
 
+    def assertRouting(self, uri, uritemplate, variables):
+        self.assertEqual((uritemplate, variables), self.router(uri))
+
     def test_standard_construction(self):
-        router = URITemplateRouter([
+        self.router = URITemplateRouter([
             URITemplate('/e/{target}'),
             URITemplate('/e/{target}{/path*}'),
             URITemplate('/e/{target}{/path*}/alternate/{view}'),
@@ -16,37 +19,54 @@ class URITemplateRouterTestCase(unittest.TestCase):
             URITemplate('/w/{target}{/path*}/index'),
         ])
 
-        self.assertEqual(('/e/{target}', {
-            'target': 'hello',
-        }), router('/e/hello'))
+        self.assertRouting(
+            '/e/hello', '/e/{target}', {
+                'target': 'hello',
+            }
+        )
 
-        self.assertEqual(('/e/{target}{/path*}', {
-            'target': 'a_target',
-            'path': ['some', 'path', 'down', 'the', 'line'],
-        }), router('/e/a_target/some/path/down/the/line'))
+        self.assertRouting(
+            '/e/a_target/some/path/down/the/line',
+            '/e/{target}{/path*}', {
+                'target': 'a_target',
+                'path': ['some', 'path', 'down', 'the', 'line'],
+            }
+        )
 
-        self.assertEqual(('/e/{target}{/path*}/alternate/{view}', {
-            'view': 'some_view',
-            'target': 'a_target',
-            'path': ['short', 'path'],
-        }), router('/e/a_target/short/path/alternate/some_view'))
+        self.assertRouting(
+            '/e/a_target/short/path/alternate/some_view',
+            '/e/{target}{/path*}/alternate/{view}', {
+                'view': 'some_view',
+                'target': 'a_target',
+                'path': ['short', 'path'],
+            }
+        )
 
-        self.assertEqual(('/w/{target}{/path*}', {
-            'target': 'other_target',
-            'path': ['some', 'other', 'path'],
-        }), router('/w/other_target/some/other/path'))
+        self.assertRouting(
+            '/w/other_target/some/other/path',
+            '/w/{target}{/path*}', {
+                'target': 'other_target',
+                'path': ['some', 'other', 'path'],
+            }
+        )
 
-        self.assertEqual(('/w/{target}{/path*}/view', {
-            'target': 'other_target',
-            'path': ['some', 'other', 'path'],
-        }), router('/w/other_target/some/other/path/view'))
+        self.assertRouting(
+            '/w/other_target/some/other/path/view',
+            '/w/{target}{/path*}/view', {
+                'target': 'other_target',
+                'path': ['some', 'other', 'path'],
+            }
+        )
 
-        self.assertIsNone(router('/e/+wat'))
-        self.assertIsNone(router('/z/nowhere/short/path/alternate/some_view'))
-        self.assertIsNone(router('/w/not_enough'))
+        self.assertIsNone(
+            self.router('/e/+wat'))
+        self.assertIsNone(
+            self.router('/z/nowhere/short/path/alternate/some_view'))
+        self.assertIsNone(
+            self.router('/w/not_enough'))
 
     def test_from_list_of_strings(self):
-        router = URITemplateRouter.from_strings([
+        self.router = URITemplateRouter.from_strings([
             '/e/{target}',
             '/e/{target}{/path*}',
             '/e/{target}{/path*}/alternate/{view}',
@@ -55,11 +75,17 @@ class URITemplateRouterTestCase(unittest.TestCase):
             '/w/{target}{/path*}/index',
         ])
 
-        self.assertEqual(('/e/{target}', {
-            'target': 'goodbye',
-        }), router('/e/goodbye'))
+        self.assertRouting(
+            '/e/goodbye',
+            '/e/{target}', {
+                'target': 'goodbye',
+            }
+        )
 
-        self.assertEqual(('/w/{target}{/path*}/index', {
-            'target': 'primary',
-            'path': ['path', 'to', 'the'],
-        }), router('/w/primary/path/to/the/index'))
+        self.assertRouting(
+            '/w/primary/path/to/the/index',
+            '/w/{target}{/path*}/index', {
+                'target': 'primary',
+                'path': ['path', 'to', 'the'],
+            }
+        )

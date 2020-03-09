05d5fb3644f061c441fbcf387f41bda5d9a81f63
Tommy Yu <tommy.yu@auckland.ac.nz>
2020-03-09 20:29:02 +1300

    Rough demo on getting a response back
    - Will need some input to test on longer jobs.

--- src/repodono/model/demo/pika_receiver.py
+++ src/repodono/model/demo/pika_receiver.py
@@ -7,16 +7,26 @@ messages to generate output at the specific location.
 
 import pika
 import json
+import traceback
 
 
 def create_connection_channel(config):
     settings = config.get('settings', {})
+
     pika_settings = config.get('pika_settings', {})
     host = pika_settings.get('host', 'localhost')
-    queue = pika_settings.get('queue', 'repodono_demo')
+    exchange = pika_settings.get('exchange', 'repodono')
+    queue = pika_settings.get('queue', 'repodono.task')
+    status = pika_settings.get('status', 'repodono.logs')
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     channel = connection.channel()
+    channel.confirm_delivery()
+
+    # the working queue
     channel.queue_declare(queue=queue)
+    # exchange for the status
+    channel.exchange_declare(exchange=exchange, exchange_type='topic')
 
     def callback(ch, method, properties, body):
         print("Received %r" % body)
@@ -24,23 +34,41 @@ def create_connection_channel(config):
             decoded = json.loads(body)
         except ValueError:
             print("Error: received invalid JSON")
+            channel.basic_reject(
+                delivery_tag=method.delivery_tag, requeue=False)
             return False
 
         # endpoint, kwargs, headers
         try:
             execution = config.request_execution(**decoded)
             result = execution()
-        except Exception:
-            print("Error: invalid message")
+        except Exception as e:
+            print("Failed execution")
+            traceback.print_exc()
+            # reject execution failures for now.
+            channel.basic_reject(
+                delivery_tag=method.delivery_tag, requeue=False)
             return False
 
-        result.store_to_disk(execution)
+        try:
+            result.store_to_disk(execution)
+        except ValueError:
+            print("Failed to serialise")
+            traceback.print_exc()
+            # reject execution failures for now.
+            channel.basic_reject(
+                delivery_tag=method.delivery_tag, requeue=False)
+            return False
+
+        channel.basic_publish(
+            exchange=exchange, routing_key=status,
+            body=str(execution.locals['__path__'])),
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def start():
         channel.basic_consume(
             queue=queue,
             on_message_callback=callback,
-            auto_ack=True,
         )
         channel.start_consuming()
 
--- src/repodono/model/demo/pika_sender.py
+++ src/repodono/model/demo/pika_sender.py
@@ -9,18 +9,50 @@ import pika
 
 
 def create_connection_channel(config):
+    # TODO figure out a common specification which this "loading" may be
+    # done in a manner that may be configured, exported/interrogated by
+    # other processes.
     settings = config.get('settings', {})
+
     pika_settings = config.get('pika_settings', {})
     host = pika_settings.get('host', 'localhost')
-    queue = pika_settings.get('queue', 'repodono_demo')
-    routing_key = pika_settings.get('routing_key', 'repodono_demo')
+    exchange = pika_settings.get('exchange', 'repodono')
+    queue = pika_settings.get('queue', 'repodono.task')
+    status = pika_settings.get('status', 'repodono.logs')
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     channel = connection.channel()
-    channel.queue_declare(queue=queue)
+    channel.confirm_delivery()
 
+    # exchange and queue for the status
+    channel.exchange_declare(exchange=exchange, exchange_type='topic')
+    q_name = channel.queue_declare(queue='', exclusive=True).method.queue
+    channel.queue_bind(exchange=exchange, queue=q_name, routing_key=status)
+
+    def status_callback(ch, method, properties, body):
+        print("status: [%r] %r" % (method.routing_key, body,))
+
+    channel.basic_consume(
+        queue=q_name, on_message_callback=status_callback, auto_ack=True)
+
+    # TODO
+    # modification of RPC - have a channel that is common that broadcast
+    # newly created resources, and the handler will stop listening when
+    # received the appropriate message, and return the thing.
     def sender(body):
         print("sending %r" % body)
-        channel.basic_publish(exchange='', routing_key=routing_key, body=body)
+        channel.basic_publish(exchange='', routing_key=queue, body=body)
+
+        # now await response
+        # connection.process_data_events()
+
+        # XXX
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            pass
+
+        connection.close()
 
     return connection, channel, sender
 

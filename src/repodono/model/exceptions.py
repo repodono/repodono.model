b44fdfa5b1e8b49ab517e08732dc85f8a0f7e021
Tommy Yu <tommy.yu@auckland.ac.nz>
2020-04-03 02:05:23 +1300

    Formalise the exception types.
    - Have this as the initial set of common possible errors.

--- src/repodono/model/exceptions.py
+++ src/repodono/model/exceptions.py
@@ -1,6 +1,25 @@
-class ExecutionNoResultError(TypeError):
+class ExecutionError(Exception):
+    """
+    Used to indicate a generic execution error.
+    """
+
+
+class ExecutionRejectError(ExecutionError):
+    """
+    This exception may be raised when an execution was rejected.
+    """
+
+
+class ExecutionNoResultError(ExecutionError, TypeError):
     """
     This exception should be raised when execution results in no valid
     result.  It should also be raised when non typed None result is
     returned.
+    """
+
+
+class ExecutionTimeoutError(ExecutionError):
+    """
+    This exception may be raised to indicate that an execution has ran
+    out of time (or otherwise timed out).
     """
